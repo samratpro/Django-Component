97e28b3bb31e2cba1726db467c205fad237a9322
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2022-07-30 17:35:05 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Creating Invirment
+python -m venv env
+
+# Active envirment
+Source scripts/env/activate
+
+
+#Commands---------------------------------------
+# Django install..
+pip install django
+
+# Django Project Create..
+django-admin startproject "projectname"
+
+# Django App Creating..
+python manage.py startapp "appname"
+
+# Server Running..
+python manage.py runserver
+
+# Database migrate..
+python manage.py migrate
+
+# Important folders..
+templates
+static
