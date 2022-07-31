5c1d6470903f67349422af24294bad96740d030d
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2022-08-01 05:37:59 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,15 @@ python manage.py startapp "appname"
 # Server Running..
 python manage.py runserver
 
-# Database migrate..
+
+## Commands
+# For database update
+python manage.py makemigrations
 python manage.py migrate
+
+# For Admin
+python manage.py create superuser
+
 
 # Important folders..
 templates
